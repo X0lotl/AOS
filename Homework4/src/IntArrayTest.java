1cf4af3194907e871699e8d58f5d1d7ff5ced240
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-03-08 15:27:17 +0200

    add all mehods to IntNumber and generate IntArray

--- Homework4/src/IntArrayTest.java
+++ Homework4/src/IntArrayTest.java
@@ -1,2 +1,31 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
 public class IntArrayTest {
+    private static void generateNewArrayInTxt() throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter("IntArray.txt"));
+        Random random = new Random();
+        for(int i = 0; i < 40000; i++){
+            out.println(random.nextInt());
+        }
+    }
+
+    private static void readArrayFromTxt(IntNumber[] arrayOfInt, int arrayLength, String txtSource){
+        arrayOfInt = new IntNumber[arrayLength];
+        Scanner scanner = new Scanner(txtSource);
+        for(int i = 0; i < arrayLength - 1; i++){
+            if(scanner.hasNext()){
+                arrayOfInt[i] = new IntNumber(scanner.nextInt());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        IntNumber[] arrayOfInt = new IntNumber[0];
+        //generateNewArrayInTxt();
+        readArrayFromTxt(arrayOfInt, 1024, "IntArray.txt");
+    }
 }
---
+++ Homework4/src/IntNumber.java
@@ -0,0 +1,16 @@
+public class IntNumber implements Comparable<IntNumber> {
+    protected int number;
+
+    public IntNumber(int number){
+        this.number = number;
+    }
+
+    @Override
+    public int compareTo(IntNumber that) {
+        return this.number - that.number;
+    }
+    @Override
+    public String toString(){
+        return "number: " + number + "\n";
+    }
+}
