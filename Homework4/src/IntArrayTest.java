cf8088ef59bc02477c4f74206409b16a9e3fc62b
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-03-08 20:45:53 +0200

    need to fix: Sorters are not work corectly with big data

--- Homework4/src/IntArrayTest.java
+++ Homework4/src/IntArrayTest.java
@@ -37,14 +37,21 @@ public class IntArrayTest {
     }
 
     public static void main(String[] args) throws IOException {
-        IntNumber[] arrayOfInt;
+        IntNumber[] arrayOfInt = {
+                new IntNumber(0),
+                new IntNumber(2),
+                new IntNumber(10),
+                new IntNumber(1000),
+                new IntNumber(-1000),
+                new IntNumber(76187)
+        };
         IntNumber[] tempArrayOfIntForSorters;
 
         //generateNewArrayInTxt();
 
         //arrayOfInt = generateNewIntArray(1024);
 
-        arrayOfInt = readArrayFromTxt(1024, "IntArray.txt");
+       // arrayOfInt = readArrayFromTxt(1024, "IntArray.txt");
 
         System.out.println("Your array before sorting: " + Arrays.toString(arrayOfInt));
 
@@ -56,12 +63,20 @@ public class IntArrayTest {
         Sorter.mergeSortComparable(tempArrayOfIntForSorters);
         time = timer.getElapsedTime();
         System.out.println('\n' + "Your array is sorted through merge sorter: \n" + Arrays.toString(tempArrayOfIntForSorters));
-        System.out.println("Merge sorter time : " + time);
+        System.out.println("Merge sorter time: " + time);
 
         tempArrayOfIntForSorters = arrayOfInt;
         timer.stopWatchAtStart();
         Sorter.quickSortComparable(tempArrayOfIntForSorters,0, tempArrayOfIntForSorters.length - 1);
         time = timer.getElapsedTime();
-        System.out.println('\n' + "Your array is sorted through quick sorter: \n");
+        System.out.println('\n' + "Your array is sorted through quick sorter: \n" + Arrays.toString(tempArrayOfIntForSorters));
+        System.out.println("Quick sorter time: " + time);
+
+        tempArrayOfIntForSorters = arrayOfInt;
+        timer.stopWatchAtStart();
+        Sorter.combSortComparable(tempArrayOfIntForSorters);
+        time = timer.getElapsedTime();
+        System.out.println('\n' + "Your array is sorted through comb sorter: \n" + Arrays.toString(tempArrayOfIntForSorters));
+        System.out.println("Comb sorter time: " + time);
     }
 }
