28af3edf27e45b7af372ae091370ae96149fc1a1
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-03-09 12:03:25 +0200

    fix bag in geration array add all methods of sort

--- Homework4/src/IntArrayTest.java
+++ Homework4/src/IntArrayTest.java
@@ -4,21 +4,25 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class IntArrayTest {
+    static final int ARRAY_LENGTH = 1024;
+    static final String PATH_TO_FILE_WITH_ARRAY_OF_INT = "IntArray.txt";
+
     private static void generateNewArrayInTxt() throws IOException {
-        PrintWriter out = new PrintWriter(new FileWriter("IntArray.txt"));
+        PrintWriter out = new PrintWriter(new FileWriter(PATH_TO_FILE_WITH_ARRAY_OF_INT));
         Random random = new Random();
-        for(int i = 0; i < 40000; i++){
-            out.print(random.nextInt() + " ");
+        for (int i = 0; i < 40000; i++) {
+            //  out.print(ARRAY_LENGTH - i + " ");
+            out.print((random.nextInt()/2) + " ");
         }
     }
 
-    private static IntNumber[] readArrayFromTxt(int arrayLength, String txtSource) throws FileNotFoundException {
-        IntNumber[] arrayOfInt = new IntNumber[arrayLength];
-        Scanner scanner = new Scanner(new File(txtSource));
+    private static IntNumber[] readArrayFromTxt() throws FileNotFoundException {
+        IntNumber[] arrayOfInt = new IntNumber[ARRAY_LENGTH];
+        Scanner scanner = new Scanner(new File(PATH_TO_FILE_WITH_ARRAY_OF_INT));
         int tempInt;
 
-        for(int i = 0; i < arrayLength; i++){
-            if(scanner.hasNext()){
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (scanner.hasNext()) {
                 arrayOfInt[i] = new IntNumber(scanner.nextInt());
             }
         }
@@ -26,10 +30,10 @@ public class IntArrayTest {
         return arrayOfInt;
     }
 
-    private static IntNumber[] generateNewIntArray(int arrayLength){
+    private static IntNumber[] generateNewIntArray(int arrayLength) {
         IntNumber[] arrayOfInt = new IntNumber[arrayLength];
         Random random = new Random();
-        for(int i = 0; i < arrayLength; i++){
+        for (int i = 0; i < arrayLength; i++) {
             arrayOfInt[i] = new IntNumber(random.nextInt());
         }
 
@@ -37,46 +41,65 @@ public class IntArrayTest {
     }
 
     public static void main(String[] args) throws IOException {
-        IntNumber[] arrayOfInt = {
-                new IntNumber(0),
-                new IntNumber(2),
-                new IntNumber(10),
-                new IntNumber(1000),
-                new IntNumber(-1000),
-                new IntNumber(76187)
-        };
-        IntNumber[] tempArrayOfIntForSorters;
 
-        //generateNewArrayInTxt();
 
-        //arrayOfInt = generateNewIntArray(1024);
+        IntNumber[] arrayOfInt;
 
-       // arrayOfInt = readArrayFromTxt(1024, "IntArray.txt");
+        generateNewArrayInTxt();
 
+        arrayOfInt = readArrayFromTxt();
         System.out.println("Your array before sorting: " + Arrays.toString(arrayOfInt));
 
         Timer timer = new Timer();
         Double time;
 
-        tempArrayOfIntForSorters = arrayOfInt;
+        arrayOfInt = readArrayFromTxt();
         timer.stopWatchAtStart();
-        Sorter.mergeSortComparable(tempArrayOfIntForSorters);
+        Sorter.mergeSortComparable(arrayOfInt);
         time = timer.getElapsedTime();
-        System.out.println('\n' + "Your array is sorted through merge sorter: \n" + Arrays.toString(tempArrayOfIntForSorters));
+        System.out.println('\n' + "Your array is sorted through merge sorter: \n" + Arrays.toString(arrayOfInt));
         System.out.println("Merge sorter time: " + time);
 
-        tempArrayOfIntForSorters = arrayOfInt;
+        arrayOfInt = readArrayFromTxt();
         timer.stopWatchAtStart();
-        Sorter.quickSortComparable(tempArrayOfIntForSorters,0, tempArrayOfIntForSorters.length - 1);
+        Sorter.quickSortComparable(arrayOfInt, 0, arrayOfInt.length - 1);
         time = timer.getElapsedTime();
-        System.out.println('\n' + "Your array is sorted through quick sorter: \n" + Arrays.toString(tempArrayOfIntForSorters));
+        System.out.println('\n' + "Your array is sorted through quick sorter: \n" + Arrays.toString(arrayOfInt));
         System.out.println("Quick sorter time: " + time);
 
-        tempArrayOfIntForSorters = arrayOfInt;
+        arrayOfInt = readArrayFromTxt();
         timer.stopWatchAtStart();
-        Sorter.combSortComparable(tempArrayOfIntForSorters);
+        Sorter.combSortComparable(arrayOfInt);
         time = timer.getElapsedTime();
-        System.out.println('\n' + "Your array is sorted through comb sorter: \n" + Arrays.toString(tempArrayOfIntForSorters));
+        System.out.println('\n' + "Your array is sorted through comb sorter: \n" + Arrays.toString(arrayOfInt));
         System.out.println("Comb sorter time: " + time);
+
+        arrayOfInt = readArrayFromTxt();
+        timer.stopWatchAtStart();
+        Sorter.insertionSortComparable(arrayOfInt);
+        time = timer.getElapsedTime();
+        System.out.println('\n' + "Your array is sorted through insertion sorter: \n" + Arrays.toString(arrayOfInt));
+        System.out.println("Insertion sorter time: " + time);
+
+        arrayOfInt = readArrayFromTxt();
+        timer.stopWatchAtStart();
+        Sorter.bubbleSortComparable(arrayOfInt);
+        time = timer.getElapsedTime();
+        System.out.println('\n' + "Your array is sorted through bubble sorter: \n" + Arrays.toString(arrayOfInt));
+        System.out.println("Bubble sorter time: " + time);
+
+        arrayOfInt = readArrayFromTxt();
+        timer.stopWatchAtStart();
+        Sorter.selectionSortComparable(arrayOfInt);
+        time = timer.getElapsedTime();
+        System.out.println('\n' + "Your array is sorted through selection sorter: \n" + Arrays.toString(arrayOfInt));
+        System.out.println("Selection sorter time: " + time);
+
+        arrayOfInt = readArrayFromTxt();
+        timer.stopWatchAtStart();
+        Sorter.shellSortComparable(arrayOfInt);
+        time = timer.getElapsedTime();
+        System.out.println('\n' + "Your array is sorted through shell sorter: \n" + Arrays.toString(arrayOfInt));
+        System.out.println("Shell sorter time: " + time);
     }
 }
