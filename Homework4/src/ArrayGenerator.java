002e743a1c033172f1a0ec61e7c1ef23099b14b4
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-03-10 11:03:42 +0200

    init ArrayGenerator.java

--- Homework4/src/ArrayGenerator.java
+++ Homework4/src/ArrayGenerator.java
@@ -1,2 +1,77 @@
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
 public class ArrayGenerator {
+    static final int ARRAY_LENGTH = 100;
+    static final String PATH_TO_FILE_WITH_ARRAY_OF_INT = "IntArray.txt";
+
+    public static void generateNewRandomArrayInTxt() throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(PATH_TO_FILE_WITH_ARRAY_OF_INT));
+        Random random = new Random();
+
+        for (int i = 0; i < 40000; i++) {
+            out.print((random.nextInt() / 2) + " ");
+        }
+
+        out.close();
+    }
+
+    public static void generateSortedArrayInTxt() throws IOException {
+        IntNumber[] intArray;
+        intArray = generateNewIntArray();
+        Sorter.mergeSortComparable(intArray);
+        PrintWriter out = new PrintWriter(new FileWriter(PATH_TO_FILE_WITH_ARRAY_OF_INT));
+
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            out.print(intArray[i] + " ");
+        }
+
+        out.close();
+    }
+
+    public static void generateNewArrayWithSameIntINTxt() throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(PATH_TO_FILE_WITH_ARRAY_OF_INT));
+
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            out.println("100 ");
+        }
+
+        out.close();
+    }
+
+    public static void generateRevertSortedArrayInTxt() throws IOException {
+        IntNumber[] arrayOfInt = generateNewIntArray();
+        Sorter.mergeSortComparable(arrayOfInt);
+        PrintWriter out = new PrintWriter(new FileWriter(PATH_TO_FILE_WITH_ARRAY_OF_INT));
+
+        for (int i = ARRAY_LENGTH - 1; i >= 0; i--) {
+            out.print(arrayOfInt[i] + " ");
+        }
+
+        out.close();
+    }
+
+    public static IntNumber[] readArrayFromTxt() throws FileNotFoundException {
+        IntNumber[] arrayOfInt = new IntNumber[ARRAY_LENGTH];
+        Scanner scanner = new Scanner(new File(PATH_TO_FILE_WITH_ARRAY_OF_INT));
+
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (scanner.hasNext()) {
+                arrayOfInt[i] = new IntNumber(scanner.nextInt());
+            }
+        }
+
+        return arrayOfInt;
+    }
+
+    public static IntNumber[] generateNewIntArray() {
+        IntNumber[] arrayOfInt = new IntNumber[ARRAY_LENGTH];
+        Random random = new Random();
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            arrayOfInt[i] = new IntNumber(random.nextInt() / 2);
+        }
+
+        return arrayOfInt;
+    }
 }
--- Homework4/src/IntArrayTest.java
+++ Homework4/src/IntArrayTest.java
@@ -5,78 +5,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class IntArrayTest {
-    static final int ARRAY_LENGTH = 100;
-    static final String PATH_TO_FILE_WITH_ARRAY_OF_INT = "IntArray.txt";
-
-    private static void generateNewRandomArrayInTxt() throws IOException {
-        PrintWriter out = new PrintWriter(new FileWriter(PATH_TO_FILE_WITH_ARRAY_OF_INT));
-        Random random = new Random();
-
-        for (int i = 0; i < 40000; i++) {
-            out.print((random.nextInt() / 2) + " ");
-        }
-
-        out.close();
-    }
-
-    private static void generateSortedArrayInTxt() throws IOException {
-        IntNumber[] intArray;
-        intArray = generateNewIntArray();
-        Sorter.mergeSortComparable(intArray);
-        PrintWriter out = new PrintWriter(new FileWriter(PATH_TO_FILE_WITH_ARRAY_OF_INT));
-
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            out.print(intArray[i] + " ");
-        }
-
-        out.close();
-    }
-
-    private static void generateNewArrayWithSameIntINTxt() throws IOException {
-        PrintWriter out = new PrintWriter(new FileWriter(PATH_TO_FILE_WITH_ARRAY_OF_INT));
-
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            out.println("100 ");
-        }
-
-        out.close();
-    }
-
-    private static void generateRevertSortedArrayInTxt() throws IOException {
-        IntNumber[] arrayOfInt = generateNewIntArray();
-        Sorter.mergeSortComparable(arrayOfInt);
-        PrintWriter out = new PrintWriter(new FileWriter(PATH_TO_FILE_WITH_ARRAY_OF_INT));
-
-        for (int i = ARRAY_LENGTH - 1; i >= 0; i--) {
-            out.print(arrayOfInt[i] + " ");
-        }
-
-        out.close();
-    }
-
-    private static IntNumber[] readArrayFromTxt() throws FileNotFoundException {
-        IntNumber[] arrayOfInt = new IntNumber[ARRAY_LENGTH];
-        Scanner scanner = new Scanner(new File(PATH_TO_FILE_WITH_ARRAY_OF_INT));
-
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            if (scanner.hasNext()) {
-                arrayOfInt[i] = new IntNumber(scanner.nextInt());
-            }
-        }
-
-        return arrayOfInt;
-    }
-
-    private static IntNumber[] generateNewIntArray() {
-        IntNumber[] arrayOfInt = new IntNumber[ARRAY_LENGTH];
-        Random random = new Random();
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            arrayOfInt[i] = new IntNumber(random.nextInt() / 2);
-        }
-
-        return arrayOfInt;
-    }
-
     public static void main(String[] args) throws IOException {
         HashMap<String, Double> sorterMethodsTime = new HashMap<>();
 
@@ -88,19 +16,19 @@ public class IntArrayTest {
 
         //generateNewRandomArrayInTxt();
 
-        generateRevertSortedArrayInTxt();
+        ArrayGenerator.generateRevertSortedArrayInTxt();
 
         //generateNewArrayWithSameIntINTxt();
 
         //generateSortedArrayInTxt();
 
-        arrayOfInt = readArrayFromTxt();
+        arrayOfInt = ArrayGenerator.readArrayFromTxt();
         System.out.println("Your array before sorting: " + Arrays.toString(arrayOfInt));
 
         Timer timer = new Timer();
         Double time;
 
-        arrayOfInt = readArrayFromTxt();
+        arrayOfInt = ArrayGenerator.readArrayFromTxt();
         timer.stopWatchAtStart();
         Sorter.mergeSortComparable(arrayOfInt);
         time = timer.getElapsedTime();
@@ -108,7 +36,7 @@ public class IntArrayTest {
         System.out.println("Merge sorter time: " + time);
         sorterMethodsTime.put("Merge sorter time", time);
 
-        arrayOfInt = readArrayFromTxt();
+        arrayOfInt = ArrayGenerator.readArrayFromTxt();
         timer.stopWatchAtStart();
         Sorter.quickSortComparable(arrayOfInt, 0, arrayOfInt.length - 1);
         time = timer.getElapsedTime();
@@ -116,7 +44,7 @@ public class IntArrayTest {
         System.out.println("Quick sorter time: " + time);
         sorterMethodsTime.put("Quick sorter time", time);
 
-        arrayOfInt = readArrayFromTxt();
+        arrayOfInt = ArrayGenerator.readArrayFromTxt();
         timer.stopWatchAtStart();
         Sorter.combSortComparable(arrayOfInt);
         time = timer.getElapsedTime();
@@ -124,7 +52,7 @@ public class IntArrayTest {
         System.out.println("Comb sorter time: " + time);
         sorterMethodsTime.put("Comb sorter time", time);
 
-        arrayOfInt = readArrayFromTxt();
+        arrayOfInt = ArrayGenerator.readArrayFromTxt();
         timer.stopWatchAtStart();
         Sorter.insertionSortComparable(arrayOfInt);
         time = timer.getElapsedTime();
@@ -132,7 +60,7 @@ public class IntArrayTest {
         System.out.println("Insertion sorter time: " + time);
         sorterMethodsTime.put("Insertion sorter time", time);
 
-        arrayOfInt = readArrayFromTxt();
+        arrayOfInt = ArrayGenerator.readArrayFromTxt();
         timer.stopWatchAtStart();
         Sorter.bubbleSortComparable(arrayOfInt);
         time = timer.getElapsedTime();
@@ -140,7 +68,7 @@ public class IntArrayTest {
         System.out.println("Bubble sorter time: " + time);
         sorterMethodsTime.put("Bubble sorter time", time);
 
-        arrayOfInt = readArrayFromTxt();
+        arrayOfInt = ArrayGenerator.readArrayFromTxt();
         timer.stopWatchAtStart();
         Sorter.selectionSortComparable(arrayOfInt);
         time = timer.getElapsedTime();
@@ -148,7 +76,7 @@ public class IntArrayTest {
         System.out.println("Selection sorter time: " + time);
         sorterMethodsTime.put("Selection sorter time", time);
 
-        arrayOfInt = readArrayFromTxt();
+        arrayOfInt = ArrayGenerator.readArrayFromTxt();
         timer.stopWatchAtStart();
         Sorter.shellSortComparable(arrayOfInt);
         time = timer.getElapsedTime();
