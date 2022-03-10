0d369c31f10c46912733a77f76e49bb000f3e87c
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-03-10 14:11:06 +0200

    init Picker

--- Homework4/src/IntArrayTest.java
+++ Homework4/src/IntArrayTest.java
@@ -3,70 +3,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class IntArrayTest {
-
-    interface PickMethod {
-        void pick(IntNumber[] arrayOfInt);
-    }
-
-    public static PickMethod[] pickMethods = new PickMethod[]{
-            new PickMethod() {
-                @Override
-                public void pick(IntNumber[] arrayOfInt) {
-                    Sorter.mergeSortComparable(arrayOfInt);
-                }
-            },
-
-            new PickMethod() {
-                @Override
-                public void pick(IntNumber[] arrayOfInt) {
-                    Sorter.quickSortComparable(arrayOfInt, 0, arrayOfInt.length - 1);
-                }
-
-            },
-
-            new PickMethod() {
-                @Override
-                public void pick(IntNumber[] arrayOfInt) {
-                    Sorter.combSortComparable(arrayOfInt);
-                }
-            },
-
-            new PickMethod() {
-                @Override
-                public void pick(IntNumber[] arrayOfInt) {
-                    Sorter.insertionSortComparable(arrayOfInt);
-                }
-            },
-
-            new PickMethod() {
-                @Override
-                public void pick(IntNumber[] arrayOfInt) {
-                    Sorter.bubbleSortComparable(arrayOfInt);
-                }
-            },
-
-            new PickMethod() {
-                @Override
-                public void pick(IntNumber[] arrayOfInt) {
-                    Sorter.bubbleSortComparable(arrayOfInt);
-                }
-            },
-
-            new PickMethod() {
-                @Override
-                public void pick(IntNumber[] arrayOfInt) {
-                    Sorter.selectionSortComparable(arrayOfInt);
-                }
-            },
-
-            new PickMethod() {
-                @Override
-                public void pick(IntNumber[] arrayOfInt) {
-                    Sorter.shellSortComparable(arrayOfInt);
-                }
-            }
-    };
-
     public static void main(String[] args) throws IOException {
         HashMap<String, Double> sorterMethodsTime = new HashMap<>();
 
@@ -93,9 +29,9 @@ public class IntArrayTest {
         Timer timer = new Timer();
         Double time;
 
-        for(int i = 0; i < pickMethods.length; i++){
+        for(int i = 0; i < Picker.pickMethods.length; i++){
             timer.stopWatchAtStart();
-            pickMethods[i].pick(arrayOfInt);
+            Picker.pickMethods[i].pick(arrayOfInt);
             time = timer.getElapsedTime();
             sorterMethodsTime.put(String.valueOf(i),time);
         }
--- Homework4/src/Picker.java
+++ Homework4/src/Picker.java
@@ -1,2 +1,64 @@
 public class Picker {
+    interface PickMethod {
+        void pick(IntNumber[] arrayOfInt);
+    }
+
+    public static PickMethod[] pickMethods = new PickMethod[]{
+            new PickMethod() {
+                @Override
+                public void pick(IntNumber[] arrayOfInt) {
+                    Sorter.mergeSortComparable(arrayOfInt);
+                }
+            },
+
+            new PickMethod() {
+                @Override
+                public void pick(IntNumber[] arrayOfInt) {
+                    Sorter.quickSortComparable(arrayOfInt, 0, arrayOfInt.length - 1);
+                }
+
+            },
+
+            new PickMethod() {
+                @Override
+                public void pick(IntNumber[] arrayOfInt) {
+                    Sorter.combSortComparable(arrayOfInt);
+                }
+            },
+
+            new PickMethod() {
+                @Override
+                public void pick(IntNumber[] arrayOfInt) {
+                    Sorter.insertionSortComparable(arrayOfInt);
+                }
+            },
+
+            new PickMethod() {
+                @Override
+                public void pick(IntNumber[] arrayOfInt) {
+                    Sorter.bubbleSortComparable(arrayOfInt);
+                }
+            },
+
+            new PickMethod() {
+                @Override
+                public void pick(IntNumber[] arrayOfInt) {
+                    Sorter.bubbleSortComparable(arrayOfInt);
+                }
+            },
+
+            new PickMethod() {
+                @Override
+                public void pick(IntNumber[] arrayOfInt) {
+                    Sorter.selectionSortComparable(arrayOfInt);
+                }
+            },
+
+            new PickMethod() {
+                @Override
+                public void pick(IntNumber[] arrayOfInt) {
+                    Sorter.shellSortComparable(arrayOfInt);
+                }
+            }
+    };
 }
