14c110668b66f446437425e1cac2a1b625420fd3
X0lotl <danil200319771@gmail.com>
2022-05-05 11:15:53 +0300

    finish 7th homework

--- Homework7/src/ManagerTest.java
+++ Homework7/src/ManagerTest.java
@@ -1,7 +1,5 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class ManagerTest {
     public static void main(String[] args) throws IOException {
@@ -11,7 +9,9 @@ public class ManagerTest {
 
         String[] testArray = {"2323", "123", "jsabdkas", "X0lotl"};
 
-        textFileManager.fileSorter(inputFile);
+        textFileManager.fileAndTwoSubStrings(inputFile,"123", "HMMMMMM");
+
+        //textFileManager.fileSorter(inputFile);
 
         //System.out.println(Arrays.toString(textFileManager.placesOfSubString("123", inputFile)));
 
--- Homework7/src/TextFileManager.java
+++ Homework7/src/TextFileManager.java
@@ -6,11 +6,12 @@ import java.util.regex.Pattern;
 public class TextFileManager {
     private PlaceOfSubString[] addPlaceOfSubString(int numberOfStringInFile, int indexOFStartOfSubStringInFile, PlaceOfSubString[] placeOfSubStrings) {
         PlaceOfSubString[] tempArray = new PlaceOfSubString[placeOfSubStrings.length + 1];
-        System.arraycopy(placeOfSubStrings,0,tempArray,0, placeOfSubStrings.length);
+        System.arraycopy(placeOfSubStrings, 0, tempArray, 0, placeOfSubStrings.length);
         tempArray[tempArray.length - 1] = new PlaceOfSubString(numberOfStringInFile, indexOFStartOfSubStringInFile);
         return tempArray;
     }
-    private String[] addStringToArray(String inputString, String[] inputArray){
+
+    private String[] addStringToArray(String inputString, String[] inputArray) {
         String[] outputArray = new String[inputArray.length + 1];
         System.arraycopy(inputArray, 0, outputArray, 0, inputArray.length);
         outputArray[outputArray.length - 1] = inputString;
@@ -22,7 +23,7 @@ public class TextFileManager {
     public String[] fileToStringArray(File inputFile) throws FileNotFoundException {
         String[] outputArray = new String[0];
         Scanner scanner = new Scanner(inputFile);
-        while (scanner.hasNext()){
+        while (scanner.hasNext()) {
             outputArray = addStringToArray(scanner.nextLine(), outputArray);
         }
 
@@ -42,8 +43,8 @@ public class TextFileManager {
 
     public boolean fileHasString(String inputString, File inputFile) throws FileNotFoundException {
         String[] inputArray = fileToStringArray(inputFile);
-        for(String stringInArray : inputArray) {
-            if(inputString.equals(stringInArray)){
+        for (String stringInArray : inputArray) {
+            if (inputString.equals(stringInArray)) {
                 return true;
             }
         }
@@ -56,7 +57,7 @@ public class TextFileManager {
         PlaceOfSubString[] placesOfSubStrings = new PlaceOfSubString[0];
         Pattern pattern = Pattern.compile(inputSubString);
 
-        for(int i = 0; i < inputArray.length; i++) {
+        for (int i = 0; i < inputArray.length; i++) {
             Matcher matcher = pattern.matcher(inputArray[i]);
             while (matcher.find()) {
                 placesOfSubStrings = addPlaceOfSubString(i, matcher.start(), placesOfSubStrings);
@@ -66,8 +67,8 @@ public class TextFileManager {
         return placesOfSubStrings;
     }
 
-    public String generateNewFileName(String oldFileName) {
-        return oldFileName+"_sorted_acs.txt";
+    public String generateNewFileName(String oldFileName, String finishOfFileName) {
+        return oldFileName + finishOfFileName;
     }
 
     public void fileSorter(File inputFile) throws IOException {
@@ -75,6 +76,18 @@ public class TextFileManager {
 
         String[] sortedStringArray = sorter.sortStringArray(fileToStringArray(inputFile));
 
-        stringArrayToFile(sortedStringArray, new File(generateNewFileName(inputFile.getName())));
+        stringArrayToFile(sortedStringArray, new File(generateNewFileName(inputFile.getName(), "_sorted_acs.txt")));
+    }
+
+    public void fileAndTwoSubStrings(File inputFile, String stringA, String stringB) throws IOException {
+        String[] inputStringArray = fileToStringArray(inputFile);
+        if(fileHasString(stringA, inputFile)){
+
+            for (int i = 0; i < inputStringArray.length; i++) {
+                inputStringArray[i] = inputStringArray[i].replace(stringA, stringB);
+            }
+
+            stringArrayToFile(inputStringArray, new File("output.txt"));
+        }
     }
 }
